a9c1884ac4c5206e0ac387515d0f270e0fbb648b
AvanishCody <72160424+AvanishCody@users.noreply.github.com>
2021-04-26 20:01:58 +0530

    Add files via upload

---
+++ createDrops.js
@@ -0,0 +1,39 @@
+class createDrops{
+
+ constructor(x,y){
+
+ var options = {
+
+  'restituion' : 0.1,
+  'friction' : 0.01
+
+ }
+
+    this.rain = Bodies.circle(x,y,5,options)
+    this.radius = 5;
+    World.add(world,this.rain);
+
+ }
+
+    updateY(){
+
+        if(this.rain.position.y > height){
+
+            Matter.Body.setPosition(this.rain, {x:random(0,500), y:random(0,650)});
+
+        }
+
+    }
+
+
+        showDrops(){
+
+            noStroke();
+            fill(0,0,225);
+            ellipseMode(CENTER);
+            ellipse(this.rain.position.x, this.rain.position.y, this.radius, this.radius);
+
+        }
+
+
+}
